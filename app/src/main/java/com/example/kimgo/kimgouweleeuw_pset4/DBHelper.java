e23e31df30ad1462a24c53b0e93cc2fa5876aaaf
GoldenSimba97 <kim.gouweleeuw@gmail.com>
2017-09-24 22:56:01 +0200

    Alles uit de SQLite filmpjes gedaan

--- app/src/main/java/com/example/kimgo/kimgouweleeuw_pset4/DBHelper.java
+++ app/src/main/java/com/example/kimgo/kimgouweleeuw_pset4/DBHelper.java
@@ -1,8 +1,12 @@
 package com.example.kimgo.kimgouweleeuw_pset4;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
 
 /**
  * Created by kimgo on 24-9-2017.
@@ -34,6 +38,65 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE);
         onCreate(db);
+    }
+
+    public void create(Contact contact) {
+        SQLiteDatabase db = getWritableDatabase();
+        onUpgrade(db, 1, 1);
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, contact.getName());
+        values.put(KEY_NUMBER, contact.getNumber());
+        db.insert(TABLE, null, values);
+        db.close();
+    }
+
+    public ArrayList<Contact> read() {
+        SQLiteDatabase db = getReadableDatabase();
+
+        // A list of costum objects to store our data
+        ArrayList<Contact> contacts = new ArrayList<>();
+
+        // Create a query to give to the cursor
+        String query = "SELECT " + KEY_ID + ", " + KEY_NAME + ", " + KEY_NUMBER + " FROM " + TABLE;
+        Cursor cursor = db.rawQuery(query, null);
+
+        // Set cursor to the beginning of our database
+        if (cursor.moveToFirst()) {
+            do {
+                // Add id, done-status and to-do from current row to to-do-list
+                String name = cursor.getString(cursor.getColumnIndex(KEY_NAME));
+                String number = cursor.getString(cursor.getColumnIndex(KEY_NUMBER));
+                int id = cursor.getInt(cursor.getColumnIndex(KEY_ID));
+
+                // Create a contact object with the newly retrieved data
+                Contact contact = new Contact(name, number, id);
+                contacts.add(contact);
+            }
+            // While there is still a next entry
+            while (cursor.moveToNext());
+        }
+
+        // Close the database and the cursor
+        cursor.close();
+        db.close();
+        return contacts;
+    }
+
+    public int update(Contact contact) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, contact.getName());
+        values.put(KEY_NUMBER, contact.getNumber());
+
+        // Return whether it has succeeded or not
+        return db.update(TABLE, values, KEY_ID + " = ? ", new String[] { String.valueOf(contact.getID())});
+    }
+
+    public void delete(Contact contact) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.delete(TABLE, " " + KEY_ID + " = ? ", new String[] { String.valueOf(contact.getID())});
+        db.close();
     }
 
 }
--- app/src/main/java/com/example/kimgo/kimgouweleeuw_pset4/MainActivity.java
+++ app/src/main/java/com/example/kimgo/kimgouweleeuw_pset4/MainActivity.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     Context context;
     DBHelper helper;
     Contact contact;
+    ArrayList<Contact> contactList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,18 @@ public class MainActivity extends AppCompatActivity {
 
         // Create a new contact to store in the database
         contact = new Contact("Mama", "0647788330");
+
+        // Create a contact in our database
+        helper.create(contact);
+
+        // Change the number and then update
+        contact.setNumber("0647788321");
+        helper.update(contact);
+
+        // Delete the contact from the database
+        helper.delete(contact);
+
+        // Ask for a list of all contacts
+        contactList = helper.read();
     }
 }
