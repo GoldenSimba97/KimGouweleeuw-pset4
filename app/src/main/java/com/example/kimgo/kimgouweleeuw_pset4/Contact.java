6d5c9f3c85d2b4e974f64b950f816462c5ad224c
GoldenSimba97 <kim.gouweleeuw@gmail.com>
2017-09-26 21:53:59 +0200

    Lettergrootte listview display aangepast, alles werkt goed!

--- app/src/main/java/com/example/kimgo/kimgouweleeuw_pset4/Contact.java
+++ app/src/main/java/com/example/kimgo/kimgouweleeuw_pset4/Contact.java
@@ -3,77 +3,40 @@ package com.example.kimgo.kimgouweleeuw_pset4;
 import java.io.Serializable;
 
 /**
- * Created by kimgo on 24-9-2017.
+ * Contact class created by kimgo on 24-9-2017.
  */
 
-public class Contact implements Serializable {
-    public String title;
+class Contact implements Serializable {
+    String title;
     private int completed;
     private int _id;
 
     // Constructor for to-do from database
-    public Contact(String todoTitle) {
+    Contact(String todoTitle) {
         title = todoTitle;
         completed = 0;
     }
 
-    public Contact(String todoTitle, int todoCompleted, int todoID) {
+    Contact(String todoTitle, int todoCompleted) {
+        title = todoTitle;
+        completed = todoCompleted;
+    }
+
+    Contact(String todoTitle, int todoCompleted, int todoID) {
         title = todoTitle;
         completed = todoCompleted;
         _id = todoID;
     }
 
     // Get the to-do title
-    public String getTitle() { return title; }
+    String getTitle() { return title; }
 
     // Get if the to-do is completed
-    public int getCompleted() { return completed; }
+    int getCompleted() { return completed; }
 
     // Get the to-do id
-    public int getID() { return _id; }
-
-    // Set the title
-    public void setTitle(String newTitle) { title = newTitle; }
+    int getID() { return _id; }
 
     // Set if to-do is completed
-    public void setCompleted() { completed = 1; }
-
-    // Set the to-do id
-    public void setID(int newID) { _id = newID; }
-
-
-//    private String name;
-//    private String phoneNumber;
-//    private int _id;
-//
-//    // Constructor for contacts from database
-//    public Contact(String contactName, String contactNumber) {
-//        name = contactName;
-//        phoneNumber = contactNumber;
-//    }
-//
-//    public Contact(String contactName, String contactNumber, int contactID) {
-//        name = contactName;
-//        phoneNumber = contactNumber;
-//        _id = contactID;
-//    }
-//
-//    // Get the contact number
-//    public String getNumber() { return phoneNumber; }
-//
-//    // Get the contact name
-//    public String getName() { return name; }
-//
-//    // Set the contact number
-//    public void setNumber(String newNumber) { phoneNumber = newNumber; }
-//
-//    // Set the contact name
-//    public void setName(String newName) { name = newName; }
-//
-//    // Get the contact id
-//    public int getID() { return _id; }
-//
-//    // Set the contact id
-//    public void setID(int newID) { _id = newID; }
-
+    void setCompleted() { completed = 1; }
 }
--- app/src/main/java/com/example/kimgo/kimgouweleeuw_pset4/MainActivity.java
+++ app/src/main/java/com/example/kimgo/kimgouweleeuw_pset4/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.kimgo.kimgouweleeuw_pset4;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -39,44 +40,35 @@ public class MainActivity extends AppCompatActivity {
         context = this;
         helper = new DBHelper(context);
 
-        // Create a new to-do item to store in the database
-//        toDo = new Contact("Shoppen");
+        Contact newTodo = new Contact("Add a new to-do by typing your to-do and clicking the ADD-TO-DO button");
+        helper.create(newTodo);
 
-        // Create a to-do item in our database
-//        helper.create(toDo);
+        Contact done = new Contact("Click on your to-do to mark it as done and it will become green", 1);
+        helper.create(done);
 
-        // Delete the contact from the database
-//        helper.delete(toDo);
+        Contact delete = new Contact("Click and hold your to-do to delete it");
+        helper.create(delete);
 
-        // Ask for a list of all to-dos
         todoList = helper.read();
 
         lvItems = (ListView) findViewById(R.id.listViewID);
 
         findViewById(R.id.addTodo).setOnClickListener(new addToDo());
 
-//        TodoAdapter todoAdapter = new TodoAdapter(this, todoList);
-////        lvItems = (ListView) findViewById(R.id.listViewID);
-//        assert lvItems != null;
-//        lvItems.setAdapter(todoAdapter);
-
+        // Set to-do to done and color it green if clicked
         lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
                 TodoAdapter todoAdapter = new TodoAdapter(mainAct, todoList);
                 Contact toDo = todoAdapter.getItem(position);
                 view.setBackgroundColor(Color.parseColor("#00C853"));
+                assert toDo != null;
                 toDo.setCompleted();
                 helper.update(toDo);
-//                String toDo = ((TextView) view).getText().toString();
-
-//                Intent intent = new Intent(view.getContext(), DeleteActivity.class);
-//                intent.putExtra("delete", track);
-//                startActivity(intent);
-//                finish();
             }
         });
 
+        // Delete to-do if it is long clicked
         lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             public boolean onItemLongClick(AdapterView<?> parent, View view,
                                     int position, long id) {
@@ -90,29 +82,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
         makeTodoAdapter();
-
-
-
-//        // Create a new contact to store in the database
-//        contact = new Contact("Mama", "0647788330");
-//
-//        // Create a contact in our database
-//        helper.create(contact);
-//
-//        // Change the number and then update
-//        contact.setNumber("0647788321");
-//        helper.update(contact);
-//
-//        // Delete the contact from the database
-//        helper.delete(contact);
-//
-//        // Ask for a list of all contacts
-//        contactList = helper.read();
     }
 
+
+    // Add to-do to to-do-list
     private class addToDo implements View.OnClickListener {
         @Override public void onClick(View view) {
             String addTodo = newTodo.getText().toString();
+            addTodo = addTodo.substring(0, 1).toUpperCase() + addTodo.substring(1);
             if (!addTodo.isEmpty()) {
                 toDo = new Contact(addTodo);
                 helper.create(toDo);
@@ -123,37 +100,33 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public class TodoAdapter extends ArrayAdapter<Contact> {
-        public TodoAdapter(Context context, ArrayList<Contact> todoList) {
+
+    // Create costum adapter for displaying the listview
+    private class TodoAdapter extends ArrayAdapter<Contact> {
+        TodoAdapter(Context context, ArrayList<Contact> todoList) {
             super(context, 0, todoList);
         }
 
+        @NonNull
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            // Get the data item for this position
+        public View getView(int position, View convertView, @NonNull ViewGroup parent) {
             Contact toDo = getItem(position);
-            // Check if an existing view is being reused, otherwise inflate the view
             if (convertView == null) {
                 convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_todo, parent, false);
             }
-            // Lookup view for data population
-            TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-            // Populate the data into the template view using the data object
+            TextView tvTitle = convertView.findViewById(R.id.tvTitle);
+            assert toDo != null;
             tvTitle.setText(toDo.title);
             if (toDo.getCompleted() == 1) {
                 convertView.setBackgroundColor(Color.parseColor("#00C853"));
             }
-            // Return the completed view to render on screen
             return convertView;
         }
     }
 
+
+    // Display all to-dos in the database in a listview
     public void makeTodoAdapter() {
-//        ArrayAdapter arrayAdapter = new ArrayAdapter<>
-//                (this, android.R.layout.simple_list_item_1, todoList);
-//        lvItems = (ListView) findViewById(R.id.listViewID);
-//        assert lvItems != null;
-//        lvItems.setAdapter(arrayAdapter);
         TodoAdapter todoAdapter = new TodoAdapter(this, todoList);
         lvItems = (ListView) findViewById(R.id.listViewID);
         assert lvItems != null;
